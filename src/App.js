5390426f6aa97d5e86fec966cba3708c985197e9
archanr <archanrupela4@gmail.com>
2024-06-18 13:50:11 -0400

    - Add draggable window

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./terminal.css";
 import logo from "./archan.png";
 import Typist from "react-typist";
 import BIRDS from 'vanta/dist/vanta.net.min'
+import ReactDOM from 'react-dom';
+import Draggable from 'react-draggable';
 
 class App extends React.Component {
   constructor() {
@@ -28,7 +30,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="app" ref={this.vantaRef}>
+        <div className="app" ref={this.vantaRef}>
         <div className="name">
           <b>Archan Rupela</b>
           <div id="content>">
@@ -36,121 +38,82 @@ class App extends React.Component {
           </div>
         </div>
         <div className="hr"></div>
-
         <div className="terminal-container">
-          <div className="terminal terminal-about">
-            <div className="terminal-header">
-              About Me
-              <div className="terminal-buttons">
-                <div className="terminal-button terminal-button-min"></div>
-                <div className="terminal-button terminal-button-max"></div>
-                <div className="terminal-button terminal-button-close"></div>
+          <Draggable name=".aboutArchan" defaultPosition={{x: 0, y: 0}} position={null} grid={[1, 1]} scale={1} onStart={this.handleStart} onDrag={this.handleDrag} onStop={this.handleStop}>
+            <div name="aboutArchan" className="terminal terminal-about">
+              <div className="terminal-header"> About Me <div className="terminal-buttons">
+                  <div className="terminal-button terminal-button-min"></div>
+                  <div className="terminal-button terminal-button-max"></div>
+                  <div className="terminal-button terminal-button-close"></div>
+                </div>
               </div>
-            </div>
-            <div>
-              <span>➜ ~ </span>
-              <Typist
-                className="TypistA"
-                avgTypingDelay={30}
-                cursor={{ hideWhenDone: true }}
-                startDelay={1500}
-              >
-                <span>cat read.txt</span>
-              </Typist>
-            </div>
-            <div>
-              <div className="showAboutMe">
-                <p>
-                    Hello! My name is Archan Rupela and welcome to my page on the internet. I'm a software engineer, game developer, and leader with two years of industry experience. Check out my published games from the link below!
-                </p>
-                <p>
-                    Chances are, you're here to learn more about my professional experience. Currently, I am a Software Engineer at American Express using continuous integration and delivery (CI/CD) design practices to improve and automate the ability to quickly release bug fixes and new features. Throughout my time there, I've gained substantial experience in GitHub Actions, CI/CD pipeline automation, identifying and fixing bugs, and writing code in different languages (Java, Python, Bash). I’m confident that all these skills are exceptionally useful to me as an engineer.
-                </p>
-                <p>
-		            I am always on the lookout for new, challenging opportunities. Feel free to contact me!
-                </p>
-                <span>➜ ~ </span> <span className="blinking_cursor_main">|</span>
-              </div>
-            </div>
-          </div>
-
-          <div className="terminal terminal-contact">
-            <div className="terminal-header">
-              Contact Info
-              <div className="terminal-buttons">
-                <div className="terminal-button terminal-button-min"></div>
-                <div className="terminal-button terminal-button-max"></div>
-                <div className="terminal-button terminal-button-close"></div>
-              </div>
-            </div>
-            <div>
-              <span>➜ ~ </span>
-              <div className="showContactMeStart">
-                <Typist
-                  className="TypistA"
-                  avgTypingDelay={30}
-                  cursor={{ hideWhenDone: true }}
-                  startDelay={5000}
-                >
-                  <span>cat contact.md</span>
+              <div>
+                <span>➜ ~ </span>
+                <Typist className="TypistA" avgTypingDelay={30} cursor={{ hideWhenDone: true }} startDelay={1500}>
+                  <span>cat read.txt</span>
                 </Typist>
               </div>
+              <div>
+                <div className="showAboutMe">
+                  <p> Hello! My name is Archan Rupela and welcome to my page on the internet. I'm a software engineer, game developer, and leader with two years of industry experience. Check out my published games from the link below! </p>
+                  <p> Chances are, you're here to learn more about my professional experience. Currently, I am a Software Engineer at American Express using continuous integration and delivery (CI/CD) design practices to improve and automate the ability to quickly release bug fixes and new features. Throughout my time there, I've gained substantial experience in GitHub Actions, CI/CD pipeline automation, identifying and fixing bugs, and writing code in different languages (Java, Python, Bash). I’m confident that all these skills are exceptionally useful to me as an engineer. </p>
+                  <p> I am always on the lookout for new, challenging opportunities. Feel free to contact me! </p>
+                  <span>➜ ~ </span>
+                  <span className="blinking_cursor_main">|</span>
+                </div>
+              </div>
             </div>
-
-            <div className="showContactMe">
-              <p>
-                <b>Resume: </b>
-                <a
-                  href="https://drive.google.com/file/d/1kBDY7M3pyUyqX18ktX5q0GY3OE3F_AlD/view?usp=sharing"
-                  target="_blank"
-                >
-                  Visit
-                </a>
-              </p>
-
-
-
-              <p>
-                <b>LinkedIn: </b>
-                <a
-                  href="https://www.linkedin.com/in/archanrupela"
-                  target="_blank"
-                >
-                  Visit
-                </a>
-                <b>  |  GitHub: </b>
-                <a href="https://github.com/archanr" target="_blank">
-                  Visit
-                </a>
-                
-                              <p>
-                <b>Email: </b>
-                <a href="mailto:archanrupela@outlook.com" >
-                  archanrupela@outlook.com
-                </a>
-              </p>
-              
-              </p>              
+          </Draggable>
+          <Draggable name=".aboutArchan" defaultPosition={{x: 0, y: 0}} position={null} grid={[1, 1]} scale={1} onStart={this.handleStart} onDrag={this.handleDrag} onStop={this.handleStop}>
+            <div name="contactArchan" className="terminal terminal-contact">
+              <div className="terminal-header"> Contact Info <div className="terminal-buttons">
+                  <div className="terminal-button terminal-button-min"></div>
+                  <div className="terminal-button terminal-button-max"></div>
+                  <div className="terminal-button terminal-button-close"></div>
+                </div>
+              </div>
+              <div>
+                <span>➜ ~ </span>
+                <div className="showContactMeStart">
+                  <Typist className="TypistA" avgTypingDelay={30} cursor={{ hideWhenDone: true }} startDelay={5000}>
+                    <span>cat contact.md</span>
+                  </Typist>
+                </div>
+              </div>
+              <div className="showContactMe">
                 <p>
-                <strong><em>Published Games: </em></strong>
-                <a href="https://apps.apple.com/us/developer/archan-rupela/id1626881374" target="_blank">
-                  iOS
-                </a>
-                <b>, </b>
-                <a href="https://play.google.com/store/apps/developer?id=Archan+Rupela" target="_blank">
-                  Android
-                </a>
-              </p>
-              
+                  <b>Resume: </b>
+                  <a href="https://drive.google.com/file/d/19COvtU1ltpJg0H98mm412ndbB0z2LeVC/view?usp=sharing" target="_blank"> Visit </a>
+                </p>
+                <p>
+                  <b>LinkedIn: </b>
+                  <a href="https://www.linkedin.com/in/archanrupela" target="_blank"> Visit </a>
+                  <b> | GitHub: </b>
+                  <a href="https://github.com/archanr" target="_blank"> Visit </a>
+                <p>
+                  <b>Email: </b>
+                  <a href="mailto:archanrupela@outlook.com"> archanrupela@outlook.com </a>
+                </p>
+                </p>
+                <p>
+                  <strong>
+                    <em>Published Games: </em>
+                  </strong>
+                  <a href="https://apps.apple.com/us/developer/archan-rupela/id1626881374" target="_blank"> iOS </a>
+                  <b>, </b>
+                  <a href="https://play.google.com/store/apps/developer?id=Archan+Rupela" target="_blank"> Android </a>
+                </p>
+              </div>
+              <div className="showContactMe">
+                <span>➜ ~ </span>
+                <span className="blinking_cursor_contact">|</span>
+              </div>
             </div>
-            <div className="showContactMe">
-              <span>➜ ~ </span> <span className="blinking_cursor_contact">|</span>
-            </div>
-          </div>
+          </Draggable>
         </div>
       </div>
     );
   }
 }
-
+// ReactDOM.render(<App/>, document.body);
 export default App;
